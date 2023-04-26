2f2eeba71d8c605a320aed4eab6a0c85e8e10bf4
Kindhee <rmichel@gaming.tech>
2023-04-26 16:30:17 +0200

    ;

--- level.py
+++ level.py
@@ -19,15 +19,11 @@ class Level():
     def update(self, game):
         
         self.TimeStart = pygame.time.get_ticks()
-                
-        # self.rect.x += self.speed
-        # if self.speed > 0 and self.rect.x + self.rect.width >= 900:
-        #     self.speed *= -1
         
-        # if self.speed < 0 and self.rect.x <= 0:
-        #     self.speed *= -1
+        for i in self.botlist:
+            i.update()
             
-        for i in range(len(self.enemies)-1, -1, -1):
+        for i in range(len(self.enemies)-1, -1, -1):   
             if self.enemies[i] < (pygame.time.get_ticks() - game.timeStart) // 1000:
                 self.enemies.pop(i)
                 self.botlist.append(Enemies())
